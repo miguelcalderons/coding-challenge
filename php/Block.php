106770a40dd39ec1a832e7e16e81099625ed23e9
Miguel Calderon <miguelcalderons@Miguels-MacBook-Pro.local>
2022-01-19 20:03:02 -0400

    format render_callback

--- php/Block.php
+++ php/Block.php
@@ -47,81 +47,83 @@ class Block {
 	 */
 	public function register_block() {
 		register_block_type_from_metadata(
-			$this->plugin->dir(),
-			[
-				'render_callback' => [ $this, 'render_callback' ],
-			]
+				$this->plugin->dir(),
+				[
+						'render_callback' => [ $this, 'render_callback' ],
+				]
 		);
 	}
 
 	/**
 	 * Renders the block.
 	 *
-	 * @param array    $attributes The attributes for the block.
-	 * @param string   $content    The block content, if any.
-	 * @param WP_Block $block      The instance of this block.
+	 * @param array $attributes The attributes for the block.
+	 * @param string $content The block content, if any.
+	 * @param WP_Block $block The instance of this block.
+	 *
 	 * @return string The markup of the block.
 	 */
 	public function render_callback( $attributes, $content, $block ) {
-		$post_types = get_post_types(  [ 'public' => true ] );
+		$post_types = get_post_types( array( 'public' => true ) );
 		$class_name = $attributes['className'];
+		$post_id    = get_the_ID();
 		ob_start();
 
 		?>
-        <div class="<?php echo $class_name; ?>">
-			<h2>Post Counts</h2>
-			<ul>
+	<div class="<?php esc_attr_e( $class_name ); ?>">
+		<h2><?php _e( 'Post Counts' ); ?></h2>
+		<ul>
 			<?php
 			foreach ( $post_types as $post_type_slug ) :
-                $post_type_object = get_post_type_object( $post_type_slug  );
-                $post_count = count(
-                    get_posts(
-						[
-							'post_type' => $post_type_slug,
-							'posts_per_page' => -1,
-						]
-					)
-                );
+				$post_type_object = get_post_type_object( $post_type_slug );
+				$post_count = wp_count_posts( $post_type_slug )->publish;
 
 				?>
-				<li><?php echo 'There are ' . $post_count . ' ' .
-					  $post_type_object->labels->name . '.'; ?></li>
-			<?php endforeach;	?>
-			</ul><p><?php echo 'The current post ID is ' . $_GET['post_id'] . '.'; ?></p>
+				<li><?php esc_html_e( 'There are ' . $post_count . ' ' .
+									  $post_type_object->labels->name . '.' ); ?></li>
+			<?php endforeach; ?>
+		</ul>
 
-			<?php
-			$query = new WP_Query(  array(
-				'post_type' => ['post', 'page'],
-				'post_status' => 'any',
-				'date_query' => array(
-					array(
-						'hour'      => 9,
-						'compare'   => '>=',
-					),
-					array(
-						'hour' => 17,
-						'compare'=> '<=',
-					),
-				),
-                'tag'  => 'foo',
-                'category_name'  => 'baz',
-				  'post__not_in' => [ get_the_ID() ],
-			));
+		<p><?php esc_html_e( 'The current post ID is ' . $post_id . '.' ); ?></p>
 
-			if ( $query->found_posts ) :
-				?>
-				 <h2>5 posts with the tag of foo and the category of baz</h2>
-                <ul>
-                <?php
-
-                 foreach ( array_slice( $query->posts, 0, 5 ) as $post ) :
-                    ?><li><?php echo $post->post_title ?></li><?php
-				endforeach;
-			endif;
-		 	?>
-			</ul>
-		</div>
 		<?php
+		$post_to_exclude = $post_id;
+		$query           = new WP_Query( array(
+				'post_type'     => array( 'post', 'page' ),
+				'post_status'   => 'any',
+				'post_per_page' => 5,
+				'date_query'    => array(
+						array(
+								'hour'    => 9,
+								'compare' => '>=',
+						),
+						array(
+								'hour'    => 17,
+								'compare' => '<=',
+						),
+				),
+				'tag'           => 'foo',
+				'category_name' => 'baz',
+		) );
+
+		if ( $query->found_posts ) :
+			?>
+			<h2><?php esc_html_e( $query->found_posts . ' posts with the tag of foo and the category of baz' ) ?> </h2>
+			<ul>
+				<?php
+				while ( $query->have_posts() ) :
+					$query->the_post();
+					if ( in_array( $post_id, $post_to_exclude ) ) {
+						continue;
+					}
+					?>
+					<li><?php esc_html_e( get_the_title() ) ?></li><?php
+				endwhile;
+				?>
+			</ul>
+			</div>
+		<?php
+		endif;
 
 		return ob_get_clean();
 	}
